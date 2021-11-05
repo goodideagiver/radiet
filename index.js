dcb1e849314ad406546e4c45c099ff868f62c217
Karol <kariurek@gmail.com>
2021-11-05 10:02:30 +0100

    fix mobile nav menu background scrolling

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function showMenu() {
     document.getElementById("mob-nav").style.left = "0";
+    document.getElementById("body").style.overflow = "hidden";
 }
 
 function hideMenu() {
     document.getElementById("mob-nav").style.left = "-100%";
+    document.getElementById("body").style.overflow = "auto";
 }
